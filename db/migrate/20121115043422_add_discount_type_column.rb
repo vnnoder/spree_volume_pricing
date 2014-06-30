296a10a614e4a2195fb1d16e8d91b54636e95aa5
Michael Bianco <iloveitaly@gmail.com>
2014-06-30 18:22:01 -0400

    Migrate existing volume prices correctly

--- db/migrate/20121115043422_add_discount_type_column.rb
+++ db/migrate/20121115043422_add_discount_type_column.rb
@@ -1,5 +1,12 @@
 class AddDiscountTypeColumn < ActiveRecord::Migration
-  def change
+  def self.up
     add_column :spree_volume_prices, :discount_type, :string
+
+    Spree::VolumePrice.reset_column_information
+    Spree::VolumePrice.all.each { |v| v.update_attribute(:discount_type, 'price') }
+  end
+
+  def self.down
+    remove_column :spree_volume_prices, :discount_type, :string
   end
 end
